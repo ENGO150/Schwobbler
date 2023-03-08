c0b09a62ba5caa23d9f50c269e8182aa0c4b2bd1
ENGO150 <v.smejkal06@gmail.com>
2023-03-08 16:18:49 +0100

    looped schwobbing

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,8 @@ fn main()
 {
     clear_terminal();
 
-    let mut slovo: String = String::new();
-    let mut upper: bool = false;
-    let mut contains_vowel: bool = false;
-    match stdin().read_line(&mut slovo)
+    let mut text: String = String::new();
+    match stdin().read_line(&mut text)
     {
         Ok(_) => {},
         Err(_) =>
@@ -30,42 +28,55 @@ fn main()
 
     clear_terminal();
 
-    //CUT NEW-LINE CHAR
-    slovo = slovo.substring(0, slovo.len() - 1).to_string();
-
-    //CHECK IF WORD STARTS WITH UPPER CASE CHAR
-    if (slovo.as_bytes()[0] as char).is_uppercase() { upper = true; }
-
-    //LOAD contains_vowel
-    for c in slovo.chars()
+    for slovo_item in text.split(' ')
     {
-        if c.is_romance_vowel()
+        let mut slovo: String = String::from(slovo_item);
+
+        let mut upper: bool = false;
+        let mut contains_vowel: bool = false;
+
+        //CUT NEW-LINE CHAR
+        if text.ends_with(slovo_item)
         {
-            contains_vowel = true;
-            break;
+            slovo = slovo.substring(0, slovo.len() - 1).to_string();
         }
+
+        //CHECK IF WORD STARTS WITH UPPER CASE CHAR
+        if (slovo.as_bytes()[0] as char).is_uppercase() { upper = true; }
+
+        //LOAD contains_vowel
+        for c in slovo.chars()
+        {
+            if c.is_romance_vowel()
+            {
+                contains_vowel = true;
+                break;
+            }
+        }
+
+        //REMOVE ANY UNWANTED PREFIX
+        if contains_vowel
+        {
+            while !(slovo.as_bytes()[0] as char).is_romance_vowel()
+            {
+                slovo = slovo.substring(1, slovo.len()).to_string();
+            }
+        } //TODO: else
+
+        slovo = slovo.to_lowercase();
+
+        slovo = "schw".to_owned() + slovo.as_str();
+
+        //MAKE FIRST CHAR CAPITAL IF upper
+        if upper
+        {
+            slovo = slovo[0..1].to_uppercase() + &slovo[1..];
+        }
+
+        print!("{slovo} ");
     }
 
-    //REMOVE ANY UNWANTED PREFIX
-    if contains_vowel
-    {
-        while !(slovo.as_bytes()[0] as char).is_romance_vowel()
-        {
-            slovo = slovo.substring(1, slovo.len()).to_string();
-        }
-    } //TODO: else
-
-    slovo = slovo.to_lowercase();
-
-    slovo = "schw".to_owned() + slovo.as_str();
-
-    //MAKE FIRST CHAR CAPITAL IF upper
-    if upper
-    {
-        slovo = slovo[0..1].to_uppercase() + &slovo[1..];
-    }
-
-    println!("{slovo}");
+    print!("\n");
 }
 
 fn clear_terminal()
