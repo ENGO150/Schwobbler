8bdfe10f72d16bdcb30109fb2f504365b67d3da9
ENGO150 <v.smejkal06@gmail.com>
2023-03-08 12:03:22 +0100

    getting user input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::io::stdin;
+
 fn main()
 {
-    println!("fuck you");
+    let mut slovo = String::new();
+    match stdin().read_line(&mut slovo)
+    {
+        Ok(_) => {},
+        Err(_) =>
+        {
+            eprintln!("poruha");
+            return;
+        }
+    };
+
+    println!("{slovo}");
 }
