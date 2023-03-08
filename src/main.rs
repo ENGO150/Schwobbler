f466697b2ece81283870372dbd70602eb454fe02
ENGO150 <v.smejkal06@gmail.com>
2023-03-08 14:17:31 +0100

    created really basic input manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
-use std::io::stdin;
+use std::
+{
+    io::
+    {
+        stdin,
+        stdout,
+        Write,
+    },
+};
+
+use substring::Substring;
 
 fn main()
 {
-    let mut slovo = String::new();
+    clear_terminal();
+    let mut slovo: String = String::new();
     match stdin().read_line(&mut slovo)
     {
         Ok(_) => {},
         Err(_) =>
         {
-            eprintln!("poruha");
-            return;
+            clear_terminal();
+            panic!("poruha");
         }
     };
 
+    clear_terminal();
+
+    slovo = slovo.substring(1, slovo.len() - 1).to_string();
+
+    slovo = "schw".to_owned() + slovo.as_str();
+
     println!("{slovo}");
+}
+
+fn clear_terminal()
+{
+    print!("\x1B[2J\x1B[1;1H");
+
+    match stdout().flush()
+    {
+        Ok(_) => {},
+        Err(_) => panic!("poruha"),
+    };
 }
